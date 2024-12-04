4a3da5e71cbcc9360ce5f1a408131d90f6ad7d99
Mohamed Elshater <107942995+Elshater6498@users.noreply.github.com>
2024-12-04 20:36:16 +0200

    cart item bg

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -53,7 +53,7 @@ const CartItem = ({ item, done = false }) => {
         <div className="relative w-full rounded-lg col-span-4 sm:col-span-3 flex items-center justify-center">
           <div>
             <img
-              className="absolute inset-0 w-full h-full p-0.5 object-cover rounded-lg"
+              className="absolute inset-0 w-full h-full p-0.5 object-contain bg-white rounded-lg"
               src={
                 BASE_URL_Img +
                 (Array.isArray(item.images)
